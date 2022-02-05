79a3ffe1f1336ecc012bc0a9b3c32202dfb9e562
Dmitry <60594670+YNDmitry@users.noreply.github.com>
2022-02-06 04:40:54 +0500

    fix

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import globalStyle from '@originjs/vite-plugin-global-style'
-import { treatAsCommonjs } from "vite-plugin-treat-umd-as-commonjs"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,6 +11,5 @@ export default defineConfig({
     globalStyle({
       sourcePath: 'src/assets/scss'
     }),
-		treatAsCommonjs()
   ],
 })
