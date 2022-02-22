528180077546a3d61f5ae90c2922cb7f984bd5fe
Dmitry <60594670+YNDmitry@users.noreply.github.com>
2022-02-22 22:18:54 +0500

    Burger (#16)
    * burger
    
    * burger update
    
    Co-authored-by: Vadim4ick <88144113+Vadim4ick@users.noreply.github.com>

---
+++ src/components/burger.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="window__burger" v-if="open">
+    <p class="burger-active__list">
+      <slot></slot>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    open: {
+      type: Boolean,
+    },
+  },
+};
+</script>
