4ae307c02cf13dab3672bcf960be6c6419f5d11e
Vadim4ick <88144113+Vadim4ick@users.noreply.github.com>
2022-02-22 22:17:26 +0500

    burger update

--- src/components/burger.vue
+++ src/components/burger.vue
@@ -1,8 +1,5 @@
 <template>
   <div class="window__burger" v-if="open">
-    <!-- <button type="button" class="popup__close" @click="$emit('close')">
-      Закрыть
-    </button> -->
     <p class="burger-active__list">
       <slot></slot>
     </p>
